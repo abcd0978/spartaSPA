897fd99b8a42d8e377648506c768e6f3a5d4c932
abcd0978 <kimmin36944@gmail.com>
2023-05-15 19:36:38 +0900

    Co-authored-by: lizzieFEstudy <lizzieFEstudy@users.noreply.github.com> Co-authored-by: Hyung-Jung Son <sonhyungjung@outlook.kr> Co-authored-by: ddooo00 <ddooo00@users.noreply.github.com>

--- app.py
+++ app.py
@@ -1,1 +1,16 @@
 #app.py
+from flask import Flask, render_template, request,jsonify
+app = Flask(__name__)
+
+@app.route('/getcomment',methods=['GET'])
+def getComment():
+   #로직작성
+   return jsonify({'result':'success','msg':'ㄹㄴㅁ'})
+
+@app.route('/writecomment',methods=['POST'])
+def writeComment():
+   #로직작성
+   return jsonify({'result':'success','msg':'ㄹㄴㅁ'})
+
+if __name__ == '__main__':  
+   app.run('0.0.0.0',port=5000,debug=True)
