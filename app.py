af90f7b5cf16ef5a8092fc0ba3ce9170200c811b
abcd0978 <kimmin36944@gmail.com>
2023-05-17 09:21:30 +0900

    first commit again, all changed

--- app.py
+++ app.py
@@ -1,14 +1,46 @@
 #app.py
 from flask import Flask, render_template, request,jsonify
+from pymongo import MongoClient
+
+client = MongoClient('mongodb+srv://kim:1234@cluster0.euyn9.mongodb.net/?retryWrites=true&w=majority')
+db = client.db
+
 app = Flask(__name__)
 
-@app.route('/getcomment',methods=['GET'])
+@app.route('/comment',methods=['GET'])
 def getComment():
    #로직작성
    return jsonify({'result':'success','msg':'ㄹㄴㅁ'})
 
-@app.route('/writecomment',methods=['POST'])
+@app.route('/comment',methods=['POST'])
 def writeComment():
+   #로직작성
+   return jsonify({'result':'success','msg':'ㄹㄴㅁ'})
+
+@app.route('/comment',methods=['DELETE'])
+def getComment():
+   #로직작성
+   comment = request.form
+   return jsonify({'result':'success','msg':'ㄹㄴㅁ'})
+
+@app.route('/comment',methods=['PUT'])
+def modifyComment():
+   #로직작성
+   commentId = request.form['commentId_give']
+   password = request.form['password_give']
+   result = db.sparta.findOne({'commentId':commentId,'password':password})
+   if(result.length == 0):
+      return jsonify({'result':'failed','msg':'no such comment'})
+
+   return jsonify({'result':'success','msg':'comment modified'})
+
+@app.route('/like',methods=['GET'])
+def getComment():
+   #로직작성
+   return jsonify({'result':'success','msg':'ㄹㄴㅁ'})
+
+@app.route('/getlike',methods=['GET'])
+def getComment():
    #로직작성
    return jsonify({'result':'success','msg':'ㄹㄴㅁ'})
 
