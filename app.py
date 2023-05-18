a9acd8b7c0e914c746ab7c796755a2eac9cfc5f6
jung <wjdtmdgk55@gmail.com>
2023-05-18 19:03:33 +0900

    좋아요기능

--- app.py
+++ app.py
@@ -7,9 +7,11 @@ client = MongoClient(  'mongodb+srv://sparta:test@cluster0.bkvgtmw.mongodb.net/r
 
 db = client.dbsparta
 
-@app.route('/')
+@app.route('/',methods=["GET","POST"])
 def home():
    return render_template('index.html')
+
+
 
 
 @app.route("/hithere", methods=["POST"])
@@ -17,21 +19,28 @@ def hi_post():
     name_receive = request.form['name_give']
     password_receive = request.form['password_give']
     message_receive = request.form['message_give']
-   
+
     doc = {
       'name': name_receive,
      'password': password_receive,
      'message': message_receive
-    }
+      }
+
+
      
     db.hi.insert_one(doc)
 
     return jsonify({'msg': '저장 완료!'})
 
+    
+    
+
+
 @app.route("/hi", methods=["GET"])
 def hi_get():
-    all_comments= list(db.hi.find({},{'_id':False}))
-    return jsonify({'result': all_comments})
+     all_comments= list(db.hi.find({},{'_id':False}))
+     return jsonify({'result': all_comments})
+    
 
 @app.route("/hi", methods=["PUT"])
 def hi_PUT():
@@ -44,5 +53,15 @@ def hi_PUT():
     return jsonify({'result': 'success'})
 
 
+
+
 if __name__ == '__main__':
    app.run('0.0.0.0', port=5000, debug=True)
+
+
+
+
+
+
+
+   
--- static/js/index.js
+++ static/js/index.js
@@ -1,7 +1,10 @@
-$(document).ready(function () {
+$(document).ready(function() {
   // set_temp();
+  getLike();
   show_comment();
 });
+
+
 
 function set_temp() {
   fetch("http://spartacodingclub.shop/sparta_api/weather/seoul")
@@ -9,6 +12,23 @@ function set_temp() {
     .then((data) => {
       let temp = data["temp"];
       $("#temp").text(temp);
+    });
+}
+function getLike(){
+  fetch("/like_get")
+    .then((res) => res.json())
+    .then((data) => {
+      for(let i=0;i<data.length; i++){
+        console.log(data);
+        console.log(data[i]);
+        let who = document.getElementById(data[i].name);
+        console.log(data[i].name+"의 카드")
+        console.log(who.nextSibling.nextSibling)
+        //let like_buttons = document.getElementsByClassName("like_btn" + i);
+        //like_buttons.textContent = team[i].like
+        //Array.from(like_buttons).forEach((el) => (el.textContent = team[i].like));
+
+      }
     });
 }
 function save_comment() {
@@ -43,6 +63,9 @@ function modify(event) {
   formData.append("password_give", password);
   formData.append("commentId_give", commentId);
   formData.append("modified_comment", modifiedComment);
+
+  
+
   fetch("/hi", { method: "PUT", body: formData })
     .then((res) => res.json())
     .then((data) => {
@@ -57,7 +80,8 @@ function show_comment() {
     .then((data) => {
       let rows = data["result"];
       $("#comment-list").empty();
-
+      
+      console.log(rows)
       rows.forEach((a) => {
         let name = a["name"];
         let commentId = a["_id"];
@@ -81,3 +105,42 @@ function show_comment() {
       });
     });
 }
+
+  async function getlikes(info) {
+    console.log('getlike 실행됨')
+    let formData = new FormData();
+    formData.append("name", info.name);
+    formData.append("like", info.like);
+    console.log(info.name,info.like)
+    await fetch("/like", { method: "POST", body:formData })
+    .then((res) => res.json())
+    .then((data) => {
+      alert(data["msg"]);
+      window.location.reload();
+    });
+  }
+
+
+
+
+  async function show_like() {
+    console.log('showlike실행됨')
+    fetch('/like_get')
+    .then(res => res.json())
+    .then((data) => {
+          console.log(data)
+          alert(data['result'].name)
+    });
+  }
+   
+
+  
+  
+   
+       
+        
+   
+  
+       
+  
+   
